3308d36d6a14112c0a4addef3cc5a0cdffc3b419
Shaheen-git767 <shahinazadskd@gmail.com>
2026-04-28 22:16:06 +0530

    added hero video and updated UI

--- server/server.js
+++ server/server.js
@@ -296,7 +296,8 @@ app.post('/api/forgot-password', async (req, res) => {
     await user.save();
 
     // const resetLink = `http://localhost:${PORT}/reset-password.html?token=${resetToken}`;
-    const resetLink = `${process.env.BASE_URL}/reset-password/${token}`;
+    const resetLink = `${process.env.BASE_URL}/reset-password.html?token=${token}`;
+
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
